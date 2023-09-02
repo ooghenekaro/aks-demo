ae6c96cf93784f370fb1fd96e7497a7241e41616
ooghenekaro <ooghenekaro@yahoo.com>
2023-09-02 05:15:01 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+# Create a resource group
+resource "azurerm_resource_group" "arg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+# Create Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "boboacr"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+# Create AKS Cluster and Grant ACR Pull acess
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "bobo_cluster"
+  location            = azurerm_resource_group.arg.location
+  resource_group_name = azurerm_resource_group.arg.name
+  dns_prefix          = "karoaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_A2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+# Create acr pull access for AKS Cluster
+resource "azurerm_role_assignment" "arm_role" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
---
+++ remote.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=remote
+STORAGE_ACCOUNT_NAME=boboguguruyole$RANDOM
+CONTAINER_NAME=containertfstate
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location westeurope
+
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
